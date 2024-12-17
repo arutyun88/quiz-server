221120aa71f631c002a991263d1d3b15967ad695
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2024-12-18 01:22:46 +0500

    add created_at update forbidden and updated_at auto updating
    closes #4

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,28 @@
 -- Для генерации UUID
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+-- Установка временной зоны БД
+SET TIMEZONE TO 'UTC';
+
+-- Создание функции автоматического обновления поля updated_at
+CREATE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Создание функции, предотвращающей изменение created_at
+CREATE OR REPLACE FUNCTION prevent_update_created_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (OLD.created_at IS DISTINCT FROM NEW.created_at) THEN RAISE EXCEPTION 'Updating "created_at" is not allowed';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 -- Создание таблицы roles
 CREATE TABLE roles
 (
@@ -48,8 +70,20 @@ CREATE TABLE tokens
 
 -- Создание таблицы question
 CREATE TABLE question (
-    id UUID PRIMARY KEY
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE TRIGGER trigger_prevent_update_created_at
+BEFORE UPDATE ON question
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_created_at();
+
+CREATE TRIGGER trigger_update_question
+BEFORE UPDATE ON question
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
 
 -- Создание таблицы question_translation
 CREATE TABLE question_translation (
@@ -57,33 +91,74 @@ CREATE TABLE question_translation (
     question_id UUID REFERENCES question(id) ON DELETE CASCADE,
     language VARCHAR(10) NOT NULL,
     text TEXT NOT NULL,
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE TRIGGER trigger_prevent_update_created_at
+BEFORE UPDATE ON question_translation
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_created_at();
+
+CREATE TRIGGER trigger_update_question_translation
+BEFORE UPDATE ON question_translation
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
 
 -- Создание таблицы answer
 CREATE TABLE answer (
     id UUID PRIMARY KEY,
     question_id UUID REFERENCES question(id) ON DELETE CASCADE,
-    is_correct BOOLEAN NOT NULL
+    is_correct BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE TRIGGER trigger_prevent_update_created_at
+BEFORE UPDATE ON answer
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_created_at();
+
+CREATE TRIGGER trigger_update_answer
+BEFORE UPDATE ON answer
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
 
 -- Создание таблицы answer_translation
 CREATE TABLE answer_translation (
     id UUID PRIMARY KEY,
     answer_id UUID REFERENCES answer(id) ON DELETE CASCADE,
     language VARCHAR(10) NOT NULL,
-    text TEXT NOT NULL
+    text TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
 );
+
+CREATE TRIGGER trigger_prevent_update_created_at
+BEFORE UPDATE ON answer_translation
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_created_at();
+
+CREATE TRIGGER trigger_update_answer_translation
+BEFORE UPDATE ON answer_translation
+FOR EACH ROW
+EXECUTE FUNCTION set_updated_at();
 
 -- Создание таблицы user_question_log
 CREATE TABLE user_question_log (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL,
     question_id UUID NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
     FOREIGN KEY (question_id) REFERENCES question (id) ON DELETE CASCADE
 );
+
+CREATE TRIGGER trigger_prevent_update_created_at
+BEFORE UPDATE ON user_question_log
+FOR EACH ROW
+EXECUTE FUNCTION prevent_update_created_at();
 
 -- Создание индекса для ускорения запроса на фильтрацию по user_id и question_id
 CREATE INDEX idx_user_question_log_user_id_question_id
