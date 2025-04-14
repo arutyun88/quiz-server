eb6db4b1029d8e2e20de3a48678777e58d752244
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2025-04-14 11:42:09 +0500

    fix birthdate dto parse format

--- src/main/java/com/arutyun/quiz_server/user/dto/UpdateUserRequestDto.java
+++ src/main/java/com/arutyun/quiz_server/user/dto/UpdateUserRequestDto.java
@@ -1,11 +1,12 @@
 package com.arutyun.quiz_server.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 
 public record UpdateUserRequestDto(
         @JsonProperty("name") String name,
-        @JsonProperty("birth_date") LocalDate birthDate
+        @JsonProperty("birth_date") @JsonFormat(pattern = "yyyy-MM-dd[ HH:mm:ss.SSS][VV]") LocalDate birthDate
 ) {
 }
